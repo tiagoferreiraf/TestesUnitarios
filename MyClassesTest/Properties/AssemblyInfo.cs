9b1741c8350c63f4e9856679d48d87009fc8abd4
tiagoalfa <tiagoferreirafonsecaa@gmail.com>
2021-08-20 14:40:50 -0300

    Add project files.

---
+++ MyClassesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyClassesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyClassesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b2e8b91-9ead-443f-be38-6b9f90e5064c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestesUnitarios/FileProcess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace TestesUnitarios
+{
+    public class FileProcess
+    {
+        public Boolean FileExists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("fileName");
+            }
+            return File.Exists(fileName);
+        }
+    }
+}
