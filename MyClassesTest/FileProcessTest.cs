fcce6623bcb0972f1ca1b8bd85e6822c2fde8006
tiagoalfa <tiagoferreirafonsecaa@gmail.com>
2021-08-20 15:01:22 -0300

    Usando AAA

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestesUnitarios;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+            Assert.IsTrue(fromCall);
+        }
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Regedit.exe");
+            Assert.IsFalse(fromCall);
+        }
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            //TODO
+            Assert.Inconclusive();
+        }
+    }
+}
